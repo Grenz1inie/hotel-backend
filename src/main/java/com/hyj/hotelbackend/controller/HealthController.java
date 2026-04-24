5cd59a31849f7fd5cc472bd385fedac8084cbc5d
Grenz1inie <3023225096@qq.com>
2026-04-24 18:35:09 +0800

    修复bug

--- src/main/java/com/hyj/hotelbackend/controller/HealthController.java
+++ src/main/java/com/hyj/hotelbackend/controller/HealthController.java
@@ -1,15 +1,9 @@
 package com.hyj.hotelbackend.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.time.OffsetDateTime;
 import java.util.Map;
 
